df3800fad48831a5731aaf721d5c53a3990faf5d
Sean Murphy <40467235+smurphy304@users.noreply.github.com>
2019-04-22 16:03:54 -0400

    Update server.R
    Shinyapps not recognizing read in location (may need further ammendments)

--- server.R
+++ server.R
@@ -33,7 +33,7 @@ shinyServer(function(input, output) {
   })
   
   ## Loading presaved ggmap (See renderplot notes)
-  load("Data/boston_img.rda")
+  load("BostonCrime/Data/boston_img.rda")
   
   
   ## Distributions tab =========================================================
